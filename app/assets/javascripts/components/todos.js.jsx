e0d1d6bac80a642aece350ea699ea145985f3651
Carlos Ramirez III <carlos@cetrasoft.com>
2014-10-29 23:13:36 -0400

    Update todo component

--- app/assets/javascripts/components/todos.js.jsx
+++ app/assets/javascripts/components/todos.js.jsx
@@ -1,103 +1,115 @@
 /** @jsx React.DOM */
 var Todo = React.createClass({
-  getInitialState: function () {
-    return {finished: this.props.finished};
+  getInitialState: function() {
+    return { finished: this.props.initialFinished };
   },
-  handleChange: function (event) {
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      type: 'PATCH',
-      data: {"todo": { finished: event.target.checked }},
-      success: function(data) {
-        this.setState({ finished: data['finished'] });
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
+  handleChanged: function(event) {
+    this.handleTodoChanged({id: this.props.id, finished: event.target.checked});
+    return;
+  },
+  // QUESTION: Which component should store the callback for updating the todo item?
+  handleTodoChanged: function(todo) {
+    this.setState({finished: todo.finished}, function() {
+      var url = '/todos/' + todo.id + '.json';
+      $.ajax({
+        url: url,
+        dataType: 'json',
+        type: 'PATCH',
+        data: {'todo': {finished: todo.finished}},
+        success: function(data) {
+          this.setState({finished: data.finished});
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
     });
-    return false;
   },
-  render: function () {
+  render: function() {
     var css = (this.state.finished) ? 'finished' : '';
     return (
       <li>
-        <input type="checkbox" checked={this.state.finished} onChange={this.handleChange}/>
-        <span className={css}>{this.props.title}</span>
+        <input type="checkbox" checked={this.state.finished} onChange={this.handleChanged}/>
+        <span className={css}>{this.props.children}</span>
       </li>
     );
   }
 });
 
-var TodoList = React.createClass({
-  render: function () {
-    var todoNodes = this.props.todos.map(function (todo, index) {
-      var todoUrl = "/todos/" + todo.id + ".json";
-      return (
-        <Todo title={todo.title} finished={todo.finished} url={todoUrl} key={index} />
-        );
-    });
-
-    return (
-      <ul className="todoList">
-        {todoNodes}
-      </ul>
-      );
-  }
-});
-
 var TodoBox = React.createClass({
-  getInitialState: function () {
-    return {todos: []};
-  },
-  componentDidMount: function () {
-    this.loadTodosFromServer();
-  },
-  loadTodosFromServer: function () {
+  loadTodosFromServer: function() {
     $.ajax({
       url: this.props.url,
       dataType: 'json',
-      success: function (todos) {
-        this.setState({todos: todos});
-      }.bind(this),
-      error: function (xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-  handleTodoSubmit: function(todo) {
-    var todos = this.state.todos;
-    var newTodos = todos.concat([todo]);
-    this.setState({todos: newTodos});
-    $.ajax({
-      url: this.props.url,
-      dataType: 'json',
-      type: 'POST',
-      data: {"todo": todo},
       success: function(data) {
-        this.loadTodosFromServer();
+        this.setState({data: data});
       }.bind(this),
       error: function(xhr, status, err) {
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
   },
-  render: function () {
+  handleTodoSubmit: function(todo) {
+    var todos = this.state.data;
+    todos.push(todo);
+    this.setState({todos: todos}, function() {
+      $.ajax({
+        url: this.props.url,
+        dataType: 'json',
+        type: 'POST',
+        data: {'todo': todo},
+        success: function(data) {
+          this.loadTodosFromServer();
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadTodosFromServer();
+  },
+  render: function() {
     return (
       <div className="todoBox">
         <TodoForm onTodoSubmit={this.handleTodoSubmit}/>
-        <TodoList todos={this.state.todos} />
+        <TodoList data={this.state.data} />
       </div>
+    );
+  }
+});
+
+var TodoList = React.createClass({
+  render: function() {
+    var todoNodes = this.props.data.map(function(todo, index) {
+      return (
+        <Todo id={todo.id} initialFinished={todo.finished} key={index}>
+          {todo.title}
+        </Todo>
       );
-    }
-  });
+    });
+    return (
+      <ul className="todoList">
+        {todoNodes}
+      </ul>
+    );
+  }
+});
 
 var TodoForm = React.createClass({
-  handleSubmit: function() {
+  handleSubmit: function(e) {
+    e.preventDefault();
     var title = this.refs.title.getDOMNode().value.trim();
+    if (!title) {
+      return;
+    }
     this.props.onTodoSubmit({title: title});
     this.refs.title.getDOMNode().value = '';
-    return false;
+    return;
   },
   render: function() {
     return (
@@ -105,11 +117,11 @@ var TodoForm = React.createClass({
         <input type="text" placeholder="New todo" ref="title" />
         <input type="submit" value="Create Todo" />
       </form>
-      );
+    );
   }
 });
 
-var ready = function () {
+var ready = function() {
   var root = document.getElementById('todos');
   var list_id = root.getAttribute('data-list-id');
   var url = "/lists/" + list_id + "/todos.json";
